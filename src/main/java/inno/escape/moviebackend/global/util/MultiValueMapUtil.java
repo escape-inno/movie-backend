9f04b6ea39e7bd2b19631196b2e2fc24aabe7468
ac2dia <ac2dia@gmail.com>
2022-04-19 23:44:17 +0900

    feat: dto to MultiValueMap converter 구현

---
+++ src/main/java/inno/escape/moviebackend/global/util/MultiValueMapUtil.java
@@ -0,0 +1,29 @@
+package inno.escape.moviebackend.global.util;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MultiValueMapUtil {
+
+  public static MultiValueMap<String, String> convert(ObjectMapper objectMapper, Object dto) {
+    try {
+      MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+      Map<String, String> map = objectMapper.convertValue(dto, new TypeReference<>() {
+      });
+      params.setAll(map);
+
+      return params;
+    } catch (IllegalArgumentException e) {
+      log.error("Url Parameter 변환중 오류가 발생했습니다. requestDto={}, {}", dto, e.toString());
+      throw new IllegalArgumentException("Url Parameter 변환중 오류가 발생했습니다.");
+    }
+  }
+}
