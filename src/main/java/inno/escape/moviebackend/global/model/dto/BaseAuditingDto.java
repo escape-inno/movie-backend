9233b1253095463fe85afd616f61c2bec2f9ea26
ac2dia <ac2dia@gmail.com>
2022-04-19 22:15:03 +0900

    feat: base model 패키지 생성

---
+++ src/main/java/inno/escape/moviebackend/global/model/dto/BaseAuditingDto.java
@@ -0,0 +1,22 @@
+package inno.escape.moviebackend.global.model.dto;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BaseAuditingDto implements Serializable {
+
+  protected String id;
+
+  protected Timestamp createdAt;
+
+  protected Timestamp updatedAt;
+}
