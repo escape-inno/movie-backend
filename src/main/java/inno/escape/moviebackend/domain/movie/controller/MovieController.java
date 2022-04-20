c7fc491884b5b0558bad4cd4c0901ee4f48dc467
ac2dia <ac2dia@gmail.com>
2022-04-21 00:27:05 +0900

    feat: 영화목록, 영화 상세정보 GET API 구현
    * Movie List
    * Movie Info

---
+++ src/main/java/inno/escape/moviebackend/domain/movie/controller/MovieController.java
@@ -0,0 +1,40 @@
+package inno.escape.moviebackend.domain.movie.controller;
+
+import inno.escape.moviebackend.domain.movie.dto.info.MovieInfoRequestDto;
+import inno.escape.moviebackend.domain.movie.dto.info.MovieInfoResponseDto;
+import inno.escape.moviebackend.domain.movie.dto.list.MovieListRequestDto;
+import inno.escape.moviebackend.domain.movie.dto.list.MovieListResponseDto;
+import inno.escape.moviebackend.domain.movie.service.MovieService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+  private final MovieService movieService;
+
+  @GetMapping("/list")
+  public ResponseEntity<Mono<MovieListResponseDto>> getCompanies(
+      @RequestBody @Valid MovieListRequestDto dto) {
+    Mono<MovieListResponseDto> mono = movieService.getMovies(dto);
+
+    return ResponseEntity.status(HttpStatus.OK).body(mono);
+  }
+
+  @GetMapping("/info")
+  public ResponseEntity<Mono<MovieInfoResponseDto>> getMovie(
+      @RequestBody @Valid MovieInfoRequestDto dto) {
+    Mono<MovieInfoResponseDto> mono = movieService.getMovie(dto);
+
+    return ResponseEntity.status(HttpStatus.OK).body(mono);
+  }
+}
---
+++ src/main/java/inno/escape/moviebackend/domain/movie/dto/info/MovieInfo.java
@@ -0,0 +1,58 @@
+package inno.escape.moviebackend.domain.movie.dto.info;
+
+import inno.escape.moviebackend.global.model.dto.Actor;
+import inno.escape.moviebackend.global.model.dto.Audit;
+import inno.escape.moviebackend.global.model.dto.Company;
+import inno.escape.moviebackend.global.model.dto.Director;
+import inno.escape.moviebackend.global.model.dto.Genre;
+import inno.escape.moviebackend.global.model.dto.Nation;
+import inno.escape.moviebackend.global.model.dto.ShowType;
+import inno.escape.moviebackend.global.model.dto.Staff;
+import java.io.Serializable;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+public class MovieInfo implements Serializable {
+
+  private String movieCd;
+
+  private String movieNm;
+
+  private String movieNmEn;
+
+  private String movieNmOg;
+
+  private String showTm;
+
+  private String prdtYear;
+
+  private String openDt;
+
+  private String prdtStatNm;
+
+  private String typeNm;
+
+  private List<Nation> nations;
+
+  private List<Genre> genres;
+
+  private List<Director> directors;
+
+  private List<Actor> actors;
+
+  private List<ShowType> showTypes;
+
+  private List<Company> companys;
+
+  private List<Audit> audits;
+
+  private List<Staff> staffs;
+}
