32c658cb34560775d709f811643bdc8d4d52e704
ac2dia <ac2dia@gmail.com>
2022-04-30 18:55:48 +0900

    feat: batch 스케쥴러, 테스크 설정

--- src/main/java/inno/escape/moviebatch/global/batch/tasklets/BoxOfficeTasklet.java
+++ src/main/java/inno/escape/moviebatch/global/batch/tasklets/BoxOfficeTasklet.java
@@ -1,16 +1,16 @@
 package inno.escape.moviebatch.global.batch.tasklets;
 
 import inno.escape.moviebatch.domain.boxoffice.dto.daily.DailyBoxOfficeRequestDto;
-import inno.escape.moviebatch.domain.boxoffice.dto.daily.DailyBoxOfficeResponseDto;
 import inno.escape.moviebatch.domain.boxoffice.service.BoxOfficeService;
 import inno.escape.moviebatch.global.Constants;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -22,15 +22,14 @@ public class BoxOfficeTasklet implements Tasklet {
   public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
     log.debug("Executed getCompanies Task");
 
+    String yesterday = DateTimeFormatter.BASIC_ISO_DATE.format(LocalDate.now().minusDays(1));
+
     DailyBoxOfficeRequestDto dto = DailyBoxOfficeRequestDto.builder()
         .key(Constants.KEY)
+        .targetDt(yesterday)
         .build();
 
-    Mono<DailyBoxOfficeResponseDto> companyListResponseDtoMono = boxOfficeService.getDailyBoxOffice(
-        dto);
-    companyListResponseDtoMono.subscribe(System.out::println);
-
-    // @TODO Mono dto 를 db 에 저장하는 작업 추가
+    boxOfficeService.getDailyBoxOffice(dto);
 
     return RepeatStatus.FINISHED;
   }
