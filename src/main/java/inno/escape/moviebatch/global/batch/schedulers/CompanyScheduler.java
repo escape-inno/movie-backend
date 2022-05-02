72561e56046d1d7a967ba29d71bc769e25ada3f4
ac2dia <ac2dia@gmail.com>
2022-05-02 19:16:08 +0900

    fix: 잘못된 push 수정

--- src/main/java/inno/escape/moviebatch/global/batch/schedulers/CompanyScheduler.java
+++ src/main/java/inno/escape/moviebatch/global/batch/schedulers/CompanyScheduler.java
@@ -1,37 +1,18 @@
 package inno.escape.moviebatch.global.batch.schedulers;
 
-<<<<<<< HEAD
 import java.time.LocalDateTime;
-<<<<<<< master
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledFuture;
-=======
-import lombok.RequiredArgsConstructor;
-=======
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ScheduledFuture;
->>>>>>> 1f4203b... feat: batch 스케쥴러, 테스크 설정
->>>>>>> feat: batch 스케쥴러, 테스크 설정
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-<<<<<<< master
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-=======
-<<<<<<< HEAD
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-=======
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.stereotype.Service;
->>>>>>> 1f4203b... feat: batch 스케쥴러, 테스크 설정
->>>>>>> feat: batch 스케쥴러, 테스크 설정
 
 @Service
 @Slf4j
@@ -41,7 +22,6 @@ public class CompanyScheduler implements SchedulerService {
   private final JobLauncher jobLauncher;
   private final TaskScheduler taskScheduler;
 
-<<<<<<< master
   private Map<String, ScheduledFuture<?>> scheduledFutureMap = new ConcurrentHashMap<>();
 
   public CompanyScheduler(@Qualifier("companyJob") Job job, JobLauncher jobLauncher,
@@ -51,11 +31,7 @@ public class CompanyScheduler implements SchedulerService {
     this.taskScheduler = taskScheduler;
   }
 
-  @Scheduled(cron = "0/20 * * * * *")
-=======
-<<<<<<< HEAD
-  @Scheduled(cron = "0 * * * * *")
->>>>>>> feat: batch 스케쥴러, 테스크 설정
+  @Scheduled(cron = "0 2 0 * * *")
   public void executeJob() {
     try {
       jobLauncher.run(
@@ -68,30 +44,6 @@ public class CompanyScheduler implements SchedulerService {
       log.error("Failed to execute job, {}", e.toString());
     }
   }
-=======
-  private Map<String, ScheduledFuture<?>> scheduledFutureMap = new ConcurrentHashMap<>();
-
-  public CompanyScheduler(@Qualifier("companyJob") Job job, JobLauncher jobLauncher,
-      TaskScheduler taskScheduler) {
-    this.job = job;
-    this.jobLauncher = jobLauncher;
-    this.taskScheduler = taskScheduler;
-  }
-
-//  @Scheduled(cron = "0 * * * * *")
-//  public void executeJob() {
-//    try {
-//      jobLauncher.run(
-//          job,
-//          new JobParametersBuilder()
-//              .addString("datetime", LocalDateTime.now().toString())
-//              .toJobParameters()  // job parameter 설정
-//      );
-//    } catch (JobExecutionException e) {
-//      log.error("Failed to execute job, {}", e.toString());
-//    }
-//  }
->>>>>>> 1f4203b... feat: batch 스케쥴러, 테스크 설정
 
   @Override
   public void register() {
